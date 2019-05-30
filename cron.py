3f67b2c525af03cf0bdf6d162b84927f4be08bd9
psb9559 <41985085+psb9559@users.noreply.github.com>
2019-05-31 01:44:43 +0900

    Add files via upload

--- cron.py
+++ cron.py
@@ -1,15 +1,19 @@
-from models import recruitinfo, test
+import urllib.parse
+import urllib.request
+from seungbot.models import *
 import requests
 from datetime import datetime
 import time
 from bs4 import BeautifulSoup
-#import math
+import math
+import xmltodict
+import json
 
-def get_subjects(url_list): # 채용 정보 크롤링 함수
+def get_subjects(api_list): # 채용 정보 크롤링 함수
 	entlist = []
 
-	for i in range(len(url_list)):
-		req = requests.get(url_list[i])
+	for i in range(len(api_list)):
+		req = requests.get(api_list[i])
 		html = req.text
 		soup = BeautifulSoup(html, 'html.parser')
 		divs1 = soup.find_all("div", {"class": "item_recruit"})
@@ -21,137 +25,219 @@ def get_subjects(url_list): # 채용 정보 크롤링 함수
 			company_grade = grade2.text
 			name = company.find("div", {"class": "area_corp"}).find("strong", {"class": "corp_name"}).find("a")
 			company_title = name.get('title')
-			try:
-				recruitinfo.objects.get(cmp_name=company_title)
-			except:
-				recruitinfo.objects.create(
-				cmp_name=company_title,
-				date=company_grade,
-				recruit_content=company_content
-				)
-
 			get_value = company.get("value")
 			entlist.append(get_value)
-			for ent_url in entlist:
-				api_url = 'http://api.saramin.co.kr/job-search?id=' + ent_url + '&fields=count'
-				html_result = requests.get(api_url)
-				result_txt = html_result.text
-				result_soup = BeautifulSoup(result_txt, 'html.parser')
-				view_name = result_soup.find("company").find("name").text
-				job_type = result_soup.find("job-type").text
-				job_level = result_soup.find("experience-level").text
-				require_level = result_soup.find("required-education-level").text
+		for ent_url in entlist:
+			api_url = 'http://api.saramin.co.kr/job-search?id=' + ent_url + '&fields=count'
+			url_id = int(ent_url)
+			try:
+				url_list.objects.get(comp_url=api_url)
+			except:
+				url_list.objects.create(
+				comp_url=api_url
+				)
+			html_result = requests.get(api_url)
+			result_txt = html_result.text
+			result_soup = BeautifulSoup(result_txt, 'html.parser')
+			view_name = result_soup.find("company").find("name").text
+			job_type = result_soup.find("job-type").text
+			job_level = result_soup.find("experience-level").text
+			require_level = result_soup.find("required-education-level").text
+
+			read_cnt = result_soup.find("read-cnt").text
+			view_cnt = int(read_cnt)
+
+			apply_cnt = result_soup.find("apply-cnt").text
+			view_apply = int(apply_cnt)
+
+			keyword = result_soup.find('keyword').text
+			salary = result_soup.find("salary").text
+
+			view_date = result_soup.find("expiration-timestamp").text
+			since_timestamp = time.mktime(datetime.today().timetuple())
+			until_timestamp = int(view_date)
+			days = (datetime.fromtimestamp(until_timestamp) - datetime.fromtimestamp(since_timestamp)).days
+
+			act_num = result_soup.find("active").text
+			view_active = int(act_num)
+
+			a = math.sqrt(view_cnt) / 1007
+			aa = math.sqrt(a)
 
 
-				read_cnt = result_soup.find("read-cnt").text
-				view_cnt = int(read_cnt)
+			b = round((view_apply / 10))
+			if b > 10:
+				result = 0.1
+			else:
+				result = switch(b)
+			result_d = 1 / days
 
-				apply_cnt = result_soup.find("apply-cnt").text
-				view_apply = int(apply_cnt)
+			info_num = aa + result + result_d
+			result_num = str(info_num)
 
-				keyword = result_soup.find('keyword').text
-				salary = result_soup.find("salary").text
+			if i == 0:
+				try:
+					recruit_info.objects.get(comp_name=view_name)
+					#get_update(view_name, view_cnt, view_apply, days, view_active)
+				except:
+					recruit_info.objects.create(
+					comp_name=view_name,
+					com_id=ent_url,
+					field='web',
+					j_type=job_type,
+					j_level=job_level,
+					r_level=require_level,
+					r_cnt=view_cnt,
+					a_cnt=view_apply,
+					k_word=keyword,
+					comp_salary=salary,
+					submit_date=days,
+					view_act=view_active,
+					score=result_num)
 
+			if i == 1:
+				try:
+					recruit_info.objects.get(comp_name=view_name)
+					#get_update(view_name, view_cnt, view_apply, days, view_active)
+				except:
+					recruit_info.objects.create(
+					comp_name=view_name,
+					com_id=ent_url,
+					field='system',
+					j_type=job_type,
+					j_level=job_level,
+					r_level=require_level,
+					r_cnt=view_cnt,
+					a_cnt=view_apply,
+					k_word=keyword,
+					comp_salary=salary,
+					submit_date=days,
+					view_act=view_active,
+					score=result_num)
 
-				view_date = result_soup.find("expiration-timestamp").text
-				since_timestamp = time.mktime(datetime.today().timetuple())
-				until_timestamp = int(view_date)
-
-				days = (datetime.fromtimestamp(until_timestamp) - datetime.fromtimestamp(since_timestamp)).days
-
-				act_num = result_soup.find("active").text
-				view_active = int(act_num)
-
-				if i == 0:
-					try:
-						test.objects.get(comp_name=view_name)
-					except:
-						test.objects.create(
-						comp_name=view_name,
-						j_type=job_type,
-						j_level=job_level,
-						r_level=require_level,
-						r_cnt=view_cnt,
-						a_cnt=view_apply,
-						k_word=keyword,
-						comp_salary=salary,
-						submit_date=days,
-						view_act=view_active,
-						field = 'web')
-
-				if i == 1:
-					try:
-						test.objects.get(comp_name=view_name)
-					except:
-						test.objects.create(
-						comp_name=view_name,
-						j_type=job_type,
-						j_level=job_level,
-						r_level=require_level,
-						r_cnt=view_cnt,
-						a_cnt=view_apply,
-						k_word=keyword,
-						comp_salary=salary,
-						submit_date=days,
-						view_act=view_active,
-						field = 'system')
-
-				if i == 2:
-					try:
-						test.objects.get(comp_name=view_name)
-					except:
-						test.objects.create(
-						comp_name=view_name,
-						j_type=job_type,
-						j_level=job_level,
-						r_level=require_level,
-						r_cnt=view_cnt,
-						a_cnt=view_apply,
-						k_word=keyword,
-						comp_salary=salary,
-						submit_date=days,
-						view_act=view_active,
-						field = 'server')
+			if i == 2:
+				try:
+					recruit_info.objects.get(comp_name=view_name)
+					#get_update(view_name, view_cnt, view_apply, days, view_active)
+				except:
+					recruit_info.objects.create(
+					comp_name=view_name,
+					com_id=ent_url,
+					field='server',
+					j_type=job_type,
+					j_level=job_level,
+					r_level=require_level,
+					r_cnt=view_cnt,
+					a_cnt=view_apply,
+					k_word=keyword,
+					comp_salary=salary,
+					submit_date=days,
+					view_act=view_active,
+					score=result_num)
 
 	return entlist
 
 
 
-url_list= ['http://www.saramin.co.kr/zf_user/search?cat_cd=404&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9&panel_type=&search_optional_item=y&search_done=y&panel_count=y',
+api_list= ['http://www.saramin.co.kr/zf_user/search?cat_cd=404&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9&panel_type=&search_optional_item=y&search_done=y&panel_count=y',
 		 'http://www.saramin.co.kr/zf_user/search?cat_cd=408&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9&panel_type=&search_optional_item=y&search_done=y&panel_count=y',
 		 'http://www.saramin.co.kr/zf_user/search?cat_cd=402&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9&panel_type=&search_optional_item=y&search_done=y&panel_count=y']
-list = get_subjects(url_list)
+list = get_subjects(api_list)
 
-# def get_rank(type):
-# 	if type == 'server':
-# 		for key in test.objects.filter(field=type):
-# 			app_cnt = key.a_cnt
-# 			view_cnt = key.r_cnt
-# 			date_cnt = key.submit_date
-# 			a = pow(view_cnt, 2) / 1007
-# 			aa = math.sqrt(a)
-# 			aaa = aa / 7
-#
-# 			b = round((app_cnt / 10))
-# 			if b > 10:
-# 				result = 0.1
-# 			else:
-# 				result = switch(b)
-# 			result_d = 1 / date_cnt
-# 			result_num = aaa + result + result_d
-#
-#
-#
-# def switch(x):
-# 	return {
-# 		1: 1,
-# 		2: 0.9,
-# 		3: 0.8,
-# 		4: 0.7,
-# 		5: 0.6,
-# 		6: 0.5,
-# 		7: 0.4,
-# 		8: 0.3,
-# 		9: 0.2,
-# 		10: 0.1,
-# 	}.get(x)
+
+def switch(x):
+	return {
+		1: 1,
+		2: 0.9,
+		3: 0.8,
+		4: 0.7,
+		5: 0.6,
+		6: 0.5,
+		7: 0.4,
+		8: 0.3,
+		9: 0.2,
+		10: 0.1,
+	}.get(x)
+
+
+def get_update():
+	for p in url_list.objects.all():
+		test_url = p.comp_url
+		request = urllib.request.Request(test_url)
+		response = urllib.request.urlopen(request)
+		jsonString = xmltodict.parse(response)
+		a = json.dumps(jsonString)
+		s = json.loads(a)
+
+		view_name = s["job-search"]["jobs"]["job"]["company"]["name"]["#text"]
+		j_cnt = s["job-search"]["jobs"]["@count"]
+		act_cnt = int(j_cnt)
+
+		read_cnt = s["job-search"]["jobs"]["job"]["read-cnt"]
+		view_cnt = int(read_cnt)
+
+		apply_cnt = s["job-search"]["jobs"]["job"]["apply-cnt"]
+		view_apply = int(apply_cnt)
+
+		view_date = s["job-search"]["jobs"]["job"]["expiration-timestamp"]
+		since_timestamp = time.mktime(datetime.today().timetuple())
+		until_timestamp = int(view_date)
+		days = (datetime.fromtimestamp(until_timestamp) - datetime.fromtimestamp(since_timestamp)).days
+
+		a = math.sqrt(view_cnt) / 1007
+		aa = math.sqrt(a)
+
+		b = round((view_apply / 10))
+		if b > 10:
+			result = 0.1
+		else:
+			result = switch(b)
+		result_d = 1 / days
+		result_num = aa + result + result_d
+
+		if act_cnt != 1:
+			delete_content = recruit_info.objects.get(comp_name=view_name)
+			delete_content.delete()
+
+		update_content = recruit_info.objects.get(comp_name=view_name)
+		if view_cnt != update_content.r_cnt:
+			update_content.r_cnt = view_cnt
+			update_content.save()
+		elif view_apply != update_content.a_cnt:
+			update_content.a_cnt = view_apply
+			update_content.save()
+		elif days != update_content.submit_date:
+			update_content.submit_date = days
+			update_content.save()
+		elif view_cnt != update_content.r_cnt and view_apply != update_content.a_cnt:
+			update_content.r_cnt = view_cnt
+			update_content.a_cnt = view_apply
+			update_content.save()
+		elif view_cnt != update_content.r_cnt and days != update_content.submit_date:
+			update_content.r_cnt = view_cnt
+			update_content.submit_date = days
+			update_content.save()
+		elif view_apply != update_content.a_cnt and days != update_content.submit_date:
+			update_content.a_cnt = view_apply
+			update_content.submit_date = days
+			update_content.save()
+		elif view_cnt != update_content.r_cnt and view_apply != update_content.a_cnt and days != update_content.submit_date:
+			update_content.r_cnt = view_cnt
+			update_content.a_cnt = view_apply
+			update_content.submit_date = days
+			update_content.save()
+		else:
+			continue
+		if result_num != update_content.score:
+			update_content.score = result_num
+			update_content.save()
+
+	return True
+
+code = get_update()
+
+
+
+
+
+
--- views.py
+++ views.py
@@ -3,176 +3,11 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse, HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 import json
-import urllib.parse
-import urllib.request
+#import urllib.parse
+#import urllib.request
 from seungbot.models import *
 import requests
 #import cron
-#from datetime import datetime
-#import time
-#from bs4 import BeautifulSoup
-#import math
-
-# def get_subjects(url_list): # 채용 정보 크롤링 함수
-# 	entlist = []
-#
-# 	for i in range(len(url_list)):
-# 		req = requests.get(url_list[i])
-# 		html = req.text
-# 		soup = BeautifulSoup(html, 'html.parser')
-# 		divs1 = soup.find_all("div", {"class": "item_recruit"})
-# 		for company in divs1:
-# 			content = company.find("div", {"class": "area_job"}).find("h2", {"class": "job_tit"}).find("a")
-# 			company_content = content.get('title')
-# 			grade1 = company.find("div", {"class": "area_job"}).find("div", {"class": "job_date"})
-# 			grade2 = grade1.find("span", {"class": "date"})
-# 			company_grade = grade2.text
-# 			name = company.find("div", {"class": "area_corp"}).find("strong", {"class": "corp_name"}).find("a")
-# 			company_title = name.get('title')
-# 			try:
-# 				recruitinfo.objects.get(cmp_name=company_title)
-# 			except:
-# 				recruitinfo.objects.create(
-# 				cmp_name=company_title,
-# 				date=company_grade,
-# 				recruit_content=company_content
-# 				)
-#
-# 			get_value = company.get("value")
-# 			entlist.append(get_value)
-# 			for ent_url in entlist:
-# 				api_url = 'http://api.saramin.co.kr/job-search?id=' + ent_url + '&fields=count'
-# 				html_result = requests.get(api_url)
-# 				result_txt = html_result.text
-# 				result_soup = BeautifulSoup(result_txt, 'html.parser')
-# 				view_name = result_soup.find("company").find("name").text
-# 				job_type = result_soup.find("job-type").text
-# 				job_level = result_soup.find("experience-level").text
-# 				require_level = result_soup.find("required-education-level").text
-#
-#
-# 				read_cnt = result_soup.find("read-cnt").text
-# 				view_cnt = int(read_cnt)
-#
-# 				apply_cnt = result_soup.find("apply-cnt").text
-# 				view_apply = int(apply_cnt)
-#
-# 				keyword = result_soup.find('keyword').text
-# 				salary = result_soup.find("salary").text
-#
-#
-# 				view_date = result_soup.find("expiration-timestamp").text
-# 				since_timestamp = time.mktime(datetime.today().timetuple())
-# 				until_timestamp = int(view_date)
-#
-# 				days = (datetime.fromtimestamp(until_timestamp) - datetime.fromtimestamp(since_timestamp)).days
-#
-# 				act_num = result_soup.find("active").text
-# 				view_active = int(act_num)
-#
-# 				if i == 0:
-# 					try:
-# 						test.objects.get(comp_name=view_name)
-# 					except:
-# 						test.objects.create(
-# 						comp_name=view_name,
-# 						j_type=job_type,
-# 						j_level=job_level,
-# 						r_level=require_level,
-# 						r_cnt=view_cnt,
-# 						a_cnt=view_apply,
-# 						k_word=keyword,
-# 						comp_salary=salary,
-# 						submit_date=days,
-# 						view_act=view_active,
-# 						field = 'web')
-#
-# 				if i == 1:
-# 					try:
-# 						test.objects.get(comp_name=view_name)
-# 					except:
-# 						test.objects.create(
-# 						comp_name=view_name,
-# 						j_type=job_type,
-# 						j_level=job_level,
-# 						r_level=require_level,
-# 						r_cnt=view_cnt,
-# 						a_cnt=view_apply,
-# 						k_word=keyword,
-# 						comp_salary=salary,
-# 						submit_date=days,
-# 						view_act=view_active,
-# 						field = 'system')
-#
-# 				if i == 2:
-# 					try:
-# 						test.objects.get(comp_name=view_name)
-# 					except:
-# 						test.objects.create(
-# 						comp_name=view_name,
-# 						j_type=job_type,
-# 						j_level=job_level,
-# 						r_level=require_level,
-# 						r_cnt=view_cnt,
-# 						a_cnt=view_apply,
-# 						k_word=keyword,
-# 						comp_salary=salary,
-# 						submit_date=days,
-# 						view_act=view_active,
-# 						field = 'server')
-#
-# 	return entlist
-#
-#
-#
-# url_list= ['http://www.saramin.co.kr/zf_user/search?cat_cd=404&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9&panel_type=&search_optional_item=y&search_done=y&panel_count=y',
-# 		 'http://www.saramin.co.kr/zf_user/search?cat_cd=408&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9&panel_type=&search_optional_item=y&search_done=y&panel_count=y',
-# 		 'http://www.saramin.co.kr/zf_user/search?cat_cd=402&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9&panel_type=&search_optional_item=y&search_done=y&panel_count=y']
-# list = get_subjects(url_list)
-#
-# def get_rank(type):
-# 	if type == 'server':
-# 		for key in test.objects.filter(field=type):
-# 			app_cnt = key.a_cnt
-# 			view_cnt = key.r_cnt
-# 			date_cnt = key.submit_date
-# 			a = pow(view_cnt, 2) / 1007
-# 			aa = math.sqrt(a)
-# 			aaa = aa / 7
-#
-# 			b = round((app_cnt / 10))
-# 			if b > 10:
-# 				result = 0.1
-# 			else:
-# 				result = switch(b)
-# 			result_d = 1 / date_cnt
-# 			result_num = aaa + result + result_d
-#
-#
-#
-#
-#
-#
-#
-#
-# def switch(x):
-# 	return {
-# 		1: 1,
-# 		2: 0.9,
-# 		3: 0.8,
-# 		4: 0.7,
-# 		5: 0.6,
-# 		6: 0.5,
-# 		7: 0.4,
-# 		8: 0.3,
-# 		9: 0.2,
-# 		10: 0.1,
-# 	}.get(x)
-#
-#
-#
-#
-#
 
 @csrf_exempt
 def message(request):
@@ -260,55 +95,55 @@ def message(request):
 
 	elif text == "서버직군 기업보여줘":
 		text = 'server'
-		#get_rank(text)
-		ep = test.objects.get(field=text)
-		if user_bot:
-			res = {
-				"version": "2.0",
-					"template": {
-						"outputs": [
-							{
-								"simpleText": {
-									"text": "회사명 :" + ep.comp_name  + "\n" + "경력/신입 :" + ep.j_level  + "\n" +"지원자수 :" + ep.a_cnt  + "\n" + "남은 마감일: " + ep.submit_date
+		ep = recruit_info.objects.raw('SELECT * FROM seungbot_recruit_info WHERE field = %s ORDER BY a_cnt DESC', [text])
+		for i in range(1, 4):
+			if user_bot:
+				res = {
+					"version": "2.0",
+						"template": {
+							"outputs": [
+								{
+									"simpleText": {
+										"text": "회사명 :" + ep[i].comp_name  + "\n" + "경력/신입 :" + ep[i].j_level  + "\n" +"지원자수 :" + ep[i].a_cnt  + "\n" + "남은 마감일: " + ep[i].submit_date
+								}
 							}
-						}
-					]
+						]
+					}
 				}
-			}
 	elif text == "웹직군 기업보여줘":
 		text = 'web'
-		#get_rank(text)
-		ep = test.objects.get(field=text)
-		if user_bot:
-			res = {
-				"version": "2.0",
-					"template": {
-						"outputs": [
-							{
-								"simpleText": {
-									"text": "회사명 :" + ep.comp_name  + "\n" + "경력/신입 :" + ep.j_level  + "\n" +"지원자수 :" + ep.a_cnt  + "\n" + "남은 마감일: " + ep.submit_date
+		ep = recruit_info.objects.raw('SELECT * FROM seungbot_recruit_info WHERE field = %s ORDER BY a_cnt DESC', [text])
+		for i in range(1, 4):
+			if user_bot:
+				res = {
+					"version": "2.0",
+						"template": {
+							"outputs": [
+								{
+									"simpleText": {
+										"text": "회사명 :" + ep[i].comp_name  + "\n" + "경력/신입 :" + ep[i].j_level  + "\n" +"지원자수 :" + ep[i].a_cnt  + "\n" + "남은 마감일: " + ep[i].submit_date
+								}
 							}
-						}
-					]
+						]
+					}
 				}
-			}
 	elif text == "시스템직군 기업보여줘":
 		text = 'system'
-		#get_rank(text)
-		ep = test.objects.get(field=text)
-		if user_bot:
-			res = {
-				"version": "2.0",
-					"template": {
-						"outputs": [
-							{
-								"simpleText": {
-									"text": "회사명 :" + ep.comp_name  + "\n" + "경력/신입 :" + ep.j_level  + "\n" +"지원자수 :" + ep.a_cnt  + "\n" + "남은 마감일: " + ep.submit_date
+		ep = recruit_info.objects.raw('SELECT * FROM seungbot_recruit_info WHERE field = %s ORDER BY a_cnt DESC', [text])
+		for i in range(1, 4):
+			if user_bot:
+				res = {
+					"version": "2.0",
+						"template": {
+							"outputs": [
+								{
+									"simpleText": {
+										"text": "회사명 :" + ep[i].comp_name  + "\n" + "경력/신입 :" + ep[i].j_level  + "\n" +"지원자수 :" + ep[i].a_cnt  + "\n" + "남은 마감일: " + ep[i].submit_date
+								}
 							}
-						}
-					]
+						]
+					}
 				}
-			}
 
 
 	return JsonResponse(res)
